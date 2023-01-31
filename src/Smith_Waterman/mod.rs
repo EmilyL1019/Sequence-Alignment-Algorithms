e53bee933c1d1213e63f7b6073c366979367f079
Emily Light <77697184+EmilyL1019@users.noreply.github.com>
2023-01-31 13:14:05 -0500

    Added tests for CLAM

--- src/Smith_Waterman/mod.rs
+++ src/Smith_Waterman/mod.rs
@@ -18,48 +18,74 @@ pub fn align(mut seq1: String, mut seq2: String) -> (Vec<String>, Vec<String>, i
     return (aligned_seq1, aligned_seq2, score); 
 }
 
-pub fn clam_align(mut seq1: String, mut seq2: String) -> u8{
-    // Convert seq1 and seq2 to string
-    // Align
-    let aligned_seq1: Vec<String>;
-    let aligned_seq2: Vec<String>;
-    let score: i32;
-    (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
-    alignment::print_alignments(&aligned_seq1, &aligned_seq2, score);
-    return score as u8;
+// Duplicate of align without print
+pub fn align_no_print(mut seq1: String, mut seq2: String) -> i32{
+    // Get the length
+    let len1 = seq1.len() as i32;
+    let len2 = seq2.len() as i32;
+    // Create the grid
+    let (grid, directions) = grid::create_grid(&mut seq1, &mut seq2, len1, len2);
+    let high_cell = alignment::highest_cell(&grid);
+    // Build and print alignment
+    let (aligned_seq1, aligned_seq2) = alignment::build_best_alignment(&grid, &directions, high_cell, &mut seq1, &mut seq2);
+    let score = alignment::score(&aligned_seq1, &aligned_seq2);
+    return score; 
+}
+
+pub fn clam_align(seq1: String, seq2: String) -> i8{
+    return align_no_print(seq1, seq2) as i8;
 }
 
 #[derive(Debug)]
-pub struct SmithWaterman {}
+/// Implements Smith-Waterman sequence alignments
+pub struct SmithWaterman {
+    pub seq1: String,
+    pub seq2: String
+}
 
-    impl <T: Number, U: Number> Metric<T, U> for SmithWaterman {
-        fn name(&self) -> String {
-            "SmithWaterman".to_string()
-        }
-
-        fn one_to_one(&self, x: &[T], y: &[T]) -> U {
-            let xu = x.iter().map(|v| v.to_be_bytes()[0]).collect();
-            let yu = y.iter().map(|v| v.to_be_bytes()[0]).collect();
-            let x_str:String = String::from_utf8(xu).unwrap();
-            let y_str:String = String::from_utf8(yu).unwrap();
-            let score = clam_align(x_str, y_str);
-            U::from(score).unwrap()
-        }
-
-        fn is_expensive(&self) -> bool {
-            true
-        }
+impl <T: Number, U: Number> Metric<T, U> for SmithWaterman {
+    /// Returns name of distance function
+    fn name(&self) -> String {
+        "SmithWaterman".to_string()
     }
+
+    /// Returns score of best Smith-Waterman alignment
+    /// 
+    /// # Arguments
+    /// 
+    /// * 'x' - An array of a unknown type representing the first sequence
+    /// * 'y' - An array of a unknown type representing the second sequence
+    fn one_to_one(&self, x: &[T], y: &[T]) -> U {
+        let xu = x.iter().map(|v| v.to_be_bytes()[0]).collect();
+        let yu = y.iter().map(|v| v.to_be_bytes()[0]).collect();
+        let x_str:String = String::from_utf8(xu).unwrap();
+        let y_str:String = String::from_utf8(yu).unwrap();
+        let score = clam_align(x_str, y_str);
+        U::from(score).unwrap()
+    }
+
+    /// Returns boolean indictating if this distance function is expensive
+    fn is_expensive(&self) -> bool {
+        true
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use clam::Metric;
     use crate::Smith_Waterman::grid::Direction;
     use crate::Smith_Waterman::grid::create_grid;
     use crate::Smith_Waterman::alignment::build_best_alignment;
     use crate::Smith_Waterman::alignment::highest_cell;
     use crate::Smith_Waterman::alignment::print_alignments;
     use crate::Smith_Waterman::alignment::score;
-    
+    use crate::Smith_Waterman::align;
+    use super::SmithWaterman;
+
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+
     #[test]
     fn test1() {
         let grid: Vec<i32> = vec![
@@ -107,7 +133,17 @@ mod tests {
         assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
         assert_eq!(score, 4);
     }
-    
+
+    #[test]
+    fn clam_ftn_test1() {
+        let seq1 : String = "GTCAGGATCT".to_string();
+        let seq2 : String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
     #[test]
     fn test2() {
         let grid = vec![0, 0, 0, 0, 0, 0,
@@ -150,7 +186,18 @@ mod tests {
         assert_eq!(aligned_seq2, vec!["TG", "TG-A", "TGAA", "GA"]);
         assert_eq!(score, 2);
     }
-   #[test]
+    
+    #[test]
+    fn clam_ftn_test2() {
+        let seq1: String = "ATGCAGGA".to_string();
+        let seq2: String = "CTGAA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TG", "TGCA", "TGCA", "GA"]);
+        assert_eq!(aligned_seq2, vec!["TG", "TG-A", "TGAA", "GA"]);
+        assert_eq!(score, 2);
+    }
+
+    #[test]
     fn test3() {
         let grid = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0,
@@ -220,6 +267,16 @@ mod tests {
     }
 
     #[test]
+    fn clam_ftn_test3() {
+        let seq1: String = "GTCAGGATCT".to_string();
+        let seq2: String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
+    #[test]
     fn test4() {
         let grid = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
@@ -259,7 +316,17 @@ mod tests {
         assert_eq!(score, 2);
     }
 
-     #[test]
+    #[test]
+    fn clam_ftn_test4() {
+        let seq1 : String = "GTCAGGATCT".to_string();
+        let seq2 : String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
+    #[test]
     fn test5() {
         let grid = vec![0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 1, 0, 0, 0,
@@ -305,6 +372,16 @@ mod tests {
         assert_eq!(score, 2);
     }
 
+    #[test]
+    fn clam_ftn_test5() {
+        let seq1 : String = "GTCAGGATCT".to_string();
+        let seq2 : String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
      #[test]
     fn test6() {
         let grid = vec![0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -348,6 +425,16 @@ mod tests {
     }
 
     #[test]
+    fn clam_ftn_test6() {
+        let seq1 : String = "GTCAGGATCT".to_string();
+        let seq2 : String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
+    #[test]
     fn test7() {
         let grid = vec![0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0,
@@ -363,8 +450,8 @@ mod tests {
         Direction::Up, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft,
         Direction::Up, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft, Direction::DiagonalUpLeft
         ];
-        let mut seq1 : String = "TTTTT".to_string();
-        let mut seq2 : String = "AAAAA".to_string();
+        let mut seq1: String = "AAAAA".to_string();
+        let mut seq2: String = "TTTTT".to_string();
         let (ftn_grid, ftn_directions) = create_grid(&mut seq1, &mut seq2, 5, 5);
         //  Check values
         for i in 0..35{
@@ -381,5 +468,92 @@ mod tests {
         let score = score(&aligned_seq1, &aligned_seq2);
         print_alignments(&aligned_seq1, &aligned_seq2, score);
         assert_eq!(score, 0);
+    }
+
+    #[test]
+    fn clam_ftn_test7() {
+        let seq1 : String = "GTCAGGATCT".to_string();
+        let seq2 : String = "ATCAAGGCCA".to_string();
+        let (aligned_seq1, aligned_seq2, score) = align(seq1, seq2);
+        assert_eq!(aligned_seq1, vec!["TC-AGG", "TCA-GG"]);
+        assert_eq!(aligned_seq2, vec!["TCAAGG", "TCAAGG"]);
+        assert_eq!(score, 4);
+    }
+
+    #[test]
+    fn struct_test1(){
+        let seq1_c: String = "GTCAGGATCT".to_string();
+        let seq2_c: String = "ATCAAGGCCA".to_string();
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 4 as i8);
+    }
+
+    #[test]
+    fn struct_test2(){
+        let seq1_c: String = "ATGCAGGA".to_string();
+        let seq2_c: String = "CTGAA".to_string();
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 2 as i8);
+    }
+
+    #[test]
+    fn struct_test3(){
+        let seq1_c: String = "AAGTAAGGTGCAGAATGAAA".to_string();
+        let seq2_c: String = "CATTCAGGAAGCTGT".to_string();        
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 5 as i8);
+    }
+
+    #[test]
+    fn struct_test4(){
+        let seq1_c: String = "TGACTG".to_string();
+        let seq2_c: String = "AAGGTACAA".to_string();
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 2 as i8);
+    }
+
+    #[test]
+    fn struct_test5(){
+        let seq1_c: String = "CTAGATGAG".to_string();
+        let seq2_c: String = "TTCAGT".to_string();
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 2 as i8);
+    }
+
+    #[test]
+    fn struct_test6(){
+        let seq1_c: String = "TTGATGT".to_string();
+        let seq2_c: String = "AAACTACA".to_string();        
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 1 as i8);
+    }
+
+    #[test]
+    fn struct_test7(){
+        let seq1_c: String = "AAAAA".to_string();
+        let seq2_c: String = "TTTTT".to_string();
+        let seq1: String = seq1_c.clone();
+        let seq2: String = seq2_c.clone();
+        let metric_sw: SmithWaterman = SmithWaterman{seq1, seq2};
+        let score:i8 = metric_sw.one_to_one(seq1_c.as_bytes(), seq2_c.as_bytes());
+        assert_eq!(score, 0 as i8);
     }
 }
